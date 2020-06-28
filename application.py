ad54dfc39939cdb3b9fb3a73030356a52065dd75
guofeng201507 <guofeng20094@gmail.com>
2020-06-28 10:28:12 +0800

    refactor code

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
+import os
+
 from flask import Flask, render_template, request, flash, redirect, url_for
 from werkzeug.utils import secure_filename
-import os
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 UPLOAD_FOLDER = './static/uploads/'
@@ -51,26 +52,6 @@ def display_image(filename):
     output_filename = filename[:-4] +'_predicted.png'
     # print('display_image filename: ' + filename)
     return redirect(url_for('static', filename='uploads/' + output_filename), code=301)
-
-
-
-
-import io
-
-import torchvision.transforms as transforms
-from PIL import Image
-
-
-def transform_image(image_bytes):
-    my_transforms = transforms.Compose([transforms.Resize(255),
-                                        transforms.CenterCrop(224),
-                                        transforms.ToTensor(),
-                                        transforms.Normalize(
-                                            [0.485, 0.456, 0.406],
-                                            [0.229, 0.224, 0.225])])
-    image = Image.open(io.BytesIO(image_bytes))
-    return my_transforms(image).unsqueeze(0)
-
 
 if __name__ == "__main__":
     app.run()
